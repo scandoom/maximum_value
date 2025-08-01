a455972425e419d4ec3d4df3f462014129bf5926
scandoom <wiwerin@yandex.ru>
2025-08-01 23:00:32 +0300

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,30 +14,123 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
+	if size <= 1 {
+		fmt.Println("error: size < 1")
+		return nil
+	}
+
+	genData := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		genData[i] = rand.Int()
+	}
+
+	return genData
 	// ваш код здесь
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
+	if len(data) <= 1 {
+		fmt.Println("error: len slice data < 1")
+		return 0
+	}
+
+	var maxCount = 0
+	for _, i := range data {
+		if i > maxCount {
+			maxCount = i
+		}
+	}
+
+	if maxCount <= 0 {
+		fmt.Println("error: maxCount <= 0")
+		return 0
+	}
+
+	return maxCount
 	// ваш код здесь
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+	if len(data) <= 1 {
+		fmt.Println("error: data < 1")
+		return 0
+	}
+
+	var wg sync.WaitGroup
+	var mu sync.RWMutex
+
+	wg.Add(8)
+
+	var maxData []int
+
+	for i := 0; i < CHUNKS; i++ {
+		v := len(data) / CHUNKS
+		if len(data)%CHUNKS != 0 {
+			fmt.Println("error: CHUNKS not correct")
+		}
+
+		partData := data[i*v : i*v+v]
+		if len(partData) <= 1 {
+			fmt.Println("error: data < 1")
+			return 0
+		}
+
+		go func(data []int) {
+			defer wg.Done()
+
+			var count = 0
+			for _, j := range data {
+				if j > count {
+					count = j
+				}
+			}
+
+			if count == 0 {
+				fmt.Println("error: count = 0")
+				return
+			}
+			mu.Lock()
+			maxData = append(maxData, count)
+			mu.Unlock()
+
+		}(partData)
+
+	}
+
+	wg.Wait()
+
+	maxCount := 0
+	for _, j := range maxData {
+		if j > maxCount {
+			maxCount = j
+		}
+	}
+
+	return maxCount
+
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+
+	general := generateRandomElements(SIZE) // ваш код здесь
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+
+	start := time.Now() // ваш код здесь
+	max := maximum(general)
+	elapsed := time.Since(start).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+
+	start = time.Now()
+	max = maxChunks(general) // ваш код здесь
+	elapsed = time.Since(start).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,43 @@
 package main
 
+import (
+	"testing"
+)
+
+func TestGenerateRandomElementsNormal(t *testing.T) {
+	for i := 2; i < CHUNKS; i++ {
+		data := generateRandomElements(i)
+		if len(data) != i {
+			t.Errorf("Expected leight %d, got %d", i, len(data))
+		}
+	}
+}
+
+func TestGenerateRandomElementsInvalid(t *testing.T) {
+	for j := -1; j < 2; j++ {
+		data := generateRandomElements(j)
+
+		if data != nil {
+			t.Errorf("error: data not nil")
+		}
+	}
+}
+
+func TestMaximumNormal(t *testing.T) {
+	data := []int{3, 78, 2, 8, 99, 98, 801}
+	max := 801
+
+	countMax := maximum(data)
+	if max != countMax {
+		t.Errorf("error: count not maximum")
+	}
+}
+
+func TestMaximumEmpty(t *testing.T) {
+	result := maximum([]int{})
+	if result != 0 {
+		t.Error("error: slice not 0")
+	}
+}
+
 // Пишите тесты в этом файле
